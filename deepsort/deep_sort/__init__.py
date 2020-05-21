655a20d749a3281be0bb1b5ee9a5cc3401a0a662
ikononykhin <ikononykhin@webim-team.ru>
2020-05-21 18:32:38 +0300

    ArcFace and MTCNN were removed

--- deepsort/deep_sort/__init__.py
+++ deepsort/deep_sort/__init__.py
@@ -13,12 +13,11 @@ N_INIT = 3
 NN_BUDGET = 100
 
 
-
-def build_tracker(use_cuda):
+def build_tracker():
     return DeepSort(REID_CKPT,
                 max_dist=MAX_DIST, min_confidence=MIN_CONFIDENCE,
                 nms_max_overlap=NMS_MAX_OVERLAP, max_iou_distance=MAX_IOU_DISTANCE,
-                max_age=MAX_AGE, n_init=N_INIT, nn_budget=NN_BUDGET, use_cuda=use_cuda)
+                max_age=MAX_AGE, n_init=N_INIT, nn_budget=NN_BUDGET)
     
 
 
